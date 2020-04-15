6e97d6e3e534c4afd7b2d07693e2fa5f5e93279c
venkatmithun <venkatmithun.k@gmail.com>
2020-04-14 17:14:09 -0700

    challenge

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cloudflare Workers Internship Application: Full-Stack
+
+## What is it?
+
+Using Cloudflare Workers, you'll deploy an application that will randomly send users to one of two webpages. This project will teach you how to write applications with the Cloudflare Workers API, manage and develop them using the command-line tool Wrangler, and deploy them to the free workers.dev deployment playground.
+
+## Published at
+
+https://fullstack.venkat.workers.dev
---
+++ index.js
@@ -0,0 +1,81 @@
+const BASE_URL = "https://cfw-takehome.developers.workers.dev/api/variants";
+
+class ElementHandler {
+  constructor(content, attr) {
+    this.content = content;
+    this.attr = attr;
+  }
+
+  element(element) {
+    element.setInnerContent(this.content);
+    const attribute = element.getAttribute(this.attr);
+    if (attribute) {
+      element.setAttribute(
+        this.attr,
+        attribute.replace(
+          "https://cloudflare.com",
+          "https://www.linkedin.com/in/venkatmithun/"
+        )
+      );
+    }
+  }
+}
+
+const rewriter = function(variant) {
+  return new HTMLRewriter()
+    .on(
+      "h1#title",
+      new ElementHandler(
+        variant == "1"
+          ? "Cloudflare Full-stack challenge"
+          : "Hey! This is Venkat Mithun"
+      )
+    )
+    .on("title", new ElementHandler(variant == "1" ? "Variant 1" : "Variant 2"))
+    .on(
+      "p#description",
+      new ElementHandler(
+        variant == "1"
+          ? "Deployed by Venkat Mithun! Cookies expire in 5 sec"
+          : "Looking for internships! Cookies expire in 5 sec"
+      )
+    )
+    .on("a#url", new ElementHandler("Interested in me?", "href"));
+};
+
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch((e) => {
+      return new Response("Error 500! Internal server error");
+    })
+  );
+});
+
+async function handleRequest(request) {
+  const response = await fetch(BASE_URL).then((res) => {
+    return res.json();
+  });
+  const variants = response["variants"];
+
+  const var1 = await fetch(variants[0], request);
+  const var2 = await fetch(variants[1], request);
+  const NAME = "variant";
+
+  const cookie = request.headers.get("cookie");
+  let group, result;
+
+  if (cookie && cookie.includes(`${NAME}=1`)) {
+    group = "1";
+    result = var1;
+  } else if (cookie && cookie.includes(`${NAME}=2`)) {
+    group = "2";
+    result = var2;
+  } else {
+    group = Math.random() < 0.5 ? "1" : "2";
+    result = group === "1" ? var1 : var2;
+    result = new Response(result.body, result);
+    // Setting cookie which expires in 5sec
+    result.headers.append("Set-Cookie", `${NAME}=${group};max-age=5; path=/`);
+  }
+  return rewriter(group).transform(result);
+}
